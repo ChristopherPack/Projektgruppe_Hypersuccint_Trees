d59c6522214aed85ebee620afe816252ca0def92
Nathanael <nathanael.stoehr@web.de>
2021-04-02 00:28:25 +0200

    WIP

---
+++ example_service/hyperTreeVisualization.h
@@ -0,0 +1,12 @@
+#ifndef PROJEKTSUCCINCTTREES_HYPERTREEVISUALIZATION_H
+#define PROJEKTSUCCINCTTREES_HYPERTREEVISUALIZATION_H
+
+#include "pht/hypersuccinct_tree.h"
+
+namespace pht {
+    class HypersuccinctTreeVisualizer {
+
+    };
+}
+
+#endif //PROJEKTSUCCINCTTREES_HYPERTREEVISUALIZATION_H
