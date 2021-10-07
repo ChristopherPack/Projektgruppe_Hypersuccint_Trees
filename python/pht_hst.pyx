21e6b1d39fa56023c252a3eb9bf161a7ad6de8c2
Nathanael <nathanael.stoehr@web.de>
2021-10-07 17:00:13 +0200

    Nath_PythonExceptionHandling: Implemented simple Exception Handling, catching IndexError exceptions thrown by C++ code

--- python/pht_hst.pyx
+++ python/pht_hst.pyx
@@ -17,47 +17,86 @@ cdef class PyHST:
         self.hst = HypersuccinctTreeFactory.create(XMLReader.read(path.encode('UTF-8')), huffman, sizeMini, sizeMicro)
 
     def isDummyAncestorWithinMiniTree(self, mini, micro, node):
-        return dereference(self.hst).isDummyAncestorWithinMiniTree(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).isDummyAncestorWithinMiniTree(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def isDummyAncestorWithinMicroTree(self, mini, micro, node):
-        return dereference(self.hst).isDummyAncestorWithinMicroTree(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).isDummyAncestorWithinMicroTree(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def child(self, mini, micro, node, index):
-        cpp_hst_node = dereference(self.hst).child(HstNode(mini, micro, node), index)
-        return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        try:
+            cpp_hst_node = dereference(self.hst).child(HstNode(mini, micro, node), index)
+            return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def childRank(self, mini, micro, node):
-        return dereference(self.hst).childRank(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).childRank(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def getParent(self, mini, micro, node):
-        cpp_hst_node = dereference(self.hst).getParent(HstNode(mini, micro, node))
-        return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        try:
+            cpp_hst_node = dereference(self.hst).getParent(HstNode(mini, micro, node))
+            return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def degree(self, mini, micro, node):
-        return dereference(self.hst).degree(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).degree(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def subtreeSize(self, mini, micro, node):
-        return dereference(self.hst).subtreeSize(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).subtreeSize(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def depth(self, mini, micro, node):
-        return dereference(self.hst).depth(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).depth(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def height(self, mini, micro, node):
-        return dereference(self.hst).height(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).height(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def leftmostLeaf(self, mini, micro, node):
-        cpp_hst_node = dereference(self.hst).leftmostLeaf(HstNode(mini, micro, node))
-        return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        try:
+            cpp_hst_node = dereference(self.hst).leftmostLeaf(HstNode(mini, micro, node))
+            return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def rightmostLeaf(self, mini, micro, node):
-        cpp_hst_node = dereference(self.hst).rightmostLeaf(HstNode(mini, micro, node))
-        return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        try:
+            cpp_hst_node = dereference(self.hst).rightmostLeaf(HstNode(mini, micro, node))
+            return (cpp_hst_node.mini, cpp_hst_node.micro, cpp_hst_node.node)
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def leafSize(self, mini, micro, node):
-        return dereference(self.hst).leafSize(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).leafSize(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
     def leafRank(self, mini, micro, node):
-        return dereference(self.hst).leafRank(HstNode(mini, micro, node))
+        try:
+            return dereference(self.hst).leafRank(HstNode(mini, micro, node))
+        except IndexError:
+            print("Vector Index out of Bounds")
 
 #   def levelAncestor(self, mini, micro, node, level):
 #        return dereference(self.hst).levelAncestor(HstNode(mini, micro, node), level)
