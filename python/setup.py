b27e29e558c78e036ccf5ecfafc7355ed36568df
Alexander Auras <alex.aur@outlook.de>
2021-10-14 19:41:58 +0200

    Improve CMake config

--- python/setup.py
+++ python/setup.py
@@ -4,10 +4,10 @@ from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(
         [Extension("pht_hst", 
-            sources=["pht_hst.pyx", "../src/bitvector_utils.cpp", "../src/hst_node.cpp", "../src/hst_output.cpp", "../src/hypersuccinct_tree.cpp", "../src/hypersuccinct_tree_queries.cpp", "../src/logger.cpp", "../src/timer.cpp", "../src/xml_reader.cpp", "../extern/irrxml/irrXML.cpp", "../extern/succinct_bv/src/bit_vector.cc"], 
-            extra_compile_args=["/std:c++17"]
+            sources=["pht_hst.pyx", "../src/bitvector_utils.cpp", "../src/hst_node.cpp", "../src/hst_output.cpp", "../src/hypersuccinct_tree.cpp", "../src/hypersuccinct_tree_queries.cpp", "../src/logger.cpp", "../src/timer.cpp", "../src/xml_reader.cpp", "../extern/irrxml/irrXML.cpp", "../extern/succinct_bv/src/bit_vector.cc", ],
+            extra_compile_args=["/std:c++17", ]
         )],
         build_dir="build"
     ), 
-    include_dirs=["../include","../extern/irrxml", "../extern/succinct_bv/include"]
+    include_dirs=["../include", "../extern/irrxml", "../extern/succinct_bv/include", ]
 )
